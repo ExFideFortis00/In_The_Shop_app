fc23881bd47adce3fcd70f07551105ec112a152c
ExFideFortis00 <Xavier.O.Stewart@gmail.com>
2014-05-09 17:32:56 -0400

    Initial ability to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
@@ -32,7 +32,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(
       :first_name,
       :last_name,
-      :email
+      :email,
+      :password,
+      :password_confirmation
       )
   end
 end
