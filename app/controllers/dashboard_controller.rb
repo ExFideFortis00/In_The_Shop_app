b554696e713a2dff357b291bcac94597789cd269
ExFideFortis00 <Xavier.O.Stewart@gmail.com>
2014-05-14 19:58:12 -0400

    Get requests from Carvoyant complete

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
     @cars = current_user.cars
     @car = Car.find(params[:car_id])
     @user = current_user
+    response = HTTParty.get('https://api.carvoyant.com/v1/api/vehicle/',:headers => { "Authorization" => "Bearer jqrqp8hkmwy4wxpdwjjrq5mn"})
+    @connected_vehicles = response["vehicle"]
   end
 
 end
